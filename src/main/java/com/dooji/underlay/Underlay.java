6015e0a920dd6f5ba176b4644fab3c0245b9cf8f
dooji2 <doojisbasement@gmail.com>
2025-05-13 00:00:46 +0300

    add support for tags

--- src/main/java/com/dooji/underlay/Underlay.java
+++ src/main/java/com/dooji/underlay/Underlay.java
@@ -1,7 +1,13 @@
 package com.dooji.underlay;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +18,8 @@ public class Underlay implements ModInitializer {
 	public static final String MOD_ID = "underlay";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private static final TagKey<Block> OVERLAY_TAG = TagKey.of(RegistryKeys.BLOCK, Identifier.of(MOD_ID, "overlay"));
+
 	@Override
 	public void onInitialize() {
 		UnderlayNetworking.init();
@@ -19,6 +27,25 @@ public class Underlay implements ModInitializer {
 		ServerWorldEvents.LOAD.register((server, world) -> {
 			LOGGER.info("Loading overlays for world: " + world.getRegistryKey().getValue());
 			UnderlayManager.loadOverlays(world);
+			reloadDatapackBlocks(world);
 		});
+
+		ServerLifecycleEvents.END_DATA_PACK_RELOAD.register((server, resourceManager, success) -> {
+            if (!success) {
+				return;
+			}
+
+            for (ServerWorld world : server.getWorlds()) {
+                reloadDatapackBlocks(world);
+            }
+        });
+	}
+
+	private static void reloadDatapackBlocks(ServerWorld world) {
+		UnderlayApi.CUSTOM_BLOCKS_DP.clear();
+		var blocks = world.getRegistryManager().getOrThrow(RegistryKeys.BLOCK);
+
+		blocks.iterateEntries(OVERLAY_TAG)
+			.forEach(entry -> UnderlayApi.registerDatapackOverlayBlock(entry.value()));
 	}
 }
