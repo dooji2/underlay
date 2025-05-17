9ddc3bf44f3cd819622e85baecc0129caeeb39ab
dooji2 <doojisbasement@gmail.com>
2025-05-17 11:57:27 +0300

    remove unused methods

--- src/client/java/com/dooji/underlay/UnderlayClient.java
+++ src/client/java/com/dooji/underlay/UnderlayClient.java
@@ -96,17 +96,6 @@ public class UnderlayClient implements ClientModInitializer {
 		wasRightDown = rightDown;
 	}
 
-	public static boolean isLookingDirectlyAtOverlay(MinecraftClient client) {
-		if (!(client.crosshairTarget instanceof BlockHitResult hit) || client.player == null) return false;
-
-		BlockHitResult overlayHit = UnderlayRaycast.trace(client.player, client.player.getBlockInteractionRange(), client.getRenderTickCounter().getTickDelta(true));
-		return overlayHit != null && overlayHit.getBlockPos().equals(hit.getBlockPos());
-	}
-
-	public static BlockPos getDirectlyTargetedOverlay(MinecraftClient client) {
-		return isLookingDirectlyAtOverlay(client) ? ((BlockHitResult)client.crosshairTarget).getBlockPos() : null;
-	}
-
 	public static BlockPos findOverlayUnderCrosshair(MinecraftClient client) {
 		if (client.player == null) return null;
 
