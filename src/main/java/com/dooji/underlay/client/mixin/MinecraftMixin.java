8598155e219673e5f78acf58a9be4734356fe9a1
dooji2 <doojisbasement@gmail.com>
2025-05-21 00:42:25 +0300

    add mixins

---
+++ src/main/java/com/dooji/underlay/client/mixin/MinecraftMixin.java
@@ -0,0 +1,25 @@
+package com.dooji.underlay.client.mixin;
+
+import com.dooji.underlay.client.UnderlayClient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+    @Inject(method = "startAttack", at = @At("HEAD"), cancellable = true)
+    private void handleInitialBreaking(CallbackInfoReturnable<Boolean> cir) {
+        Minecraft client = Minecraft.getInstance();
+        BlockPos overlayPos = UnderlayClient.findOverlayUnderCrosshair(client);
+
+        if (overlayPos != null) {
+            UnderlayClient.breakOverlay(client, overlayPos);
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}
