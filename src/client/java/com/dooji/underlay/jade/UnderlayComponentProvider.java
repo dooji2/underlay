f3825ed12883f90a16aeb67395b0b105ff4c235e
dooji2 <doojisbasement@gmail.com>
2025-06-20 13:34:24 +0300

    remove jade & set correct version

--- src/client/java/com/dooji/underlay/jade/UnderlayComponentProvider.java
+++ src/client/java/com/dooji/underlay/jade/UnderlayComponentProvider.java
@@ -1,39 +1,39 @@
-package com.dooji.underlay.jade;
+// package com.dooji.underlay.jade;
 
-import com.dooji.underlay.Underlay;
-import com.dooji.underlay.UnderlayRaycast;
+// import com.dooji.underlay.Underlay;
+// import com.dooji.underlay.UnderlayRaycast;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import snownee.jade.api.BlockAccessor;
-import snownee.jade.api.IBlockComponentProvider;
-import snownee.jade.api.ITooltip;
-import snownee.jade.api.config.IPluginConfig;
+// import net.minecraft.client.MinecraftClient;
+// import net.minecraft.text.Text;
+// import net.minecraft.util.Identifier;
+// import net.minecraft.util.hit.BlockHitResult;
+// import net.minecraft.util.math.BlockPos;
+// import snownee.jade.api.BlockAccessor;
+// import snownee.jade.api.IBlockComponentProvider;
+// import snownee.jade.api.ITooltip;
+// import snownee.jade.api.config.IPluginConfig;
 
-public enum UnderlayComponentProvider implements IBlockComponentProvider {
-    INSTANCE;
+// public enum UnderlayComponentProvider implements IBlockComponentProvider {
+//     INSTANCE;
 
-    @Override
-    public Identifier getUid() {
-        return Identifier.of(Underlay.MOD_ID, "overlay");
-    }
+//     @Override
+//     public Identifier getUid() {
+//         return Identifier.of(Underlay.MOD_ID, "overlay");
+//     }
 
-    @Override
-    public int getDefaultPriority() {
-        return -100;
-    }
+//     @Override
+//     public int getDefaultPriority() {
+//         return -100;
+//     }
 
-    @Override
-    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
-        BlockPos pos = accessor.getPosition();
-        MinecraftClient client = MinecraftClient.getInstance();
+//     @Override
+//     public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+//         BlockPos pos = accessor.getPosition();
+//         MinecraftClient client = MinecraftClient.getInstance();
 
-        BlockHitResult hit = UnderlayRaycast.trace(client.player, client.player.getBlockInteractionRange(), client.getRenderTickCounter().getTickProgress(true));
-        if (hit != null && hit.getBlockPos().equals(pos)) {
-            tooltip.append(Text.translatable("block.underlay.overlay"));
-        }
-    }
-}
+//         BlockHitResult hit = UnderlayRaycast.trace(client.player, client.player.getBlockInteractionRange(), client.getRenderTickCounter().getTickDelta(true));
+//         if (hit != null && hit.getBlockPos().equals(pos)) {
+//             tooltip.append(Text.translatable("block.underlay.overlay"));
+//         }
+//     }
+// }
