9e66ab069bd3c90bc4c8b5cd8f228acb3c7d21a2
dooji2 <doojisbasement@gmail.com>
2025-06-21 21:13:56 +0300

    oopsieeeeeeeeee

---
+++ src/main/java/com/dooji/underlay/main/Underlay.java
@@ -0,0 +1,53 @@
+package com.dooji.underlay.main;
+
+import com.dooji.underlay.main.events.BlockInteractionEvents;
+import com.dooji.underlay.main.events.PlayerEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(Underlay.MOD_ID)
+public class Underlay {
+    public static final String MOD_ID = "underlay";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static final TagKey<Block> OVERLAY_TAG = TagKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(MOD_ID, "overlay"));
+    public static final TagKey<Block> EXCLUDE_TAG = TagKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(MOD_ID, "exclude"));
+
+    public Underlay() {
+        NeoForge.EVENT_BUS.register(this);
+        NeoForge.EVENT_BUS.register(new BlockInteractionEvents());
+        NeoForge.EVENT_BUS.register(new PlayerEvents());
+    }
+
+    @SubscribeEvent
+    public void onServerStarted(ServerStartedEvent event) {
+        for (ServerLevel world : event.getServer().getAllLevels()) {
+            LOGGER.info("Loading overlays for world: " + world.dimension().location());
+            UnderlayManager.loadOverlays(world);
+            reloadDatapackBlocks(world);
+        }
+    }
+
+    private static void reloadDatapackBlocks(ServerLevel world) {
+        UnderlayApi.CUSTOM_BLOCKS_DP.clear();
+        var blocks = world.registryAccess().registryOrThrow(BuiltInRegistries.BLOCK.key());
+
+        blocks.getTag(OVERLAY_TAG).ifPresent(tag -> tag.forEach(entry -> {
+            Block block = entry.value();
+            if (!blocks.getOrCreateTag(EXCLUDE_TAG).contains(entry)) {
+                UnderlayApi.registerDatapackOverlayBlock(block);
+            }
+        }));
+    }
+}
