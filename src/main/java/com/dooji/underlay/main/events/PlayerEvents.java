b9e865c996ecae93019d91059bb4e7f6d82cbc95
dooji2 <doojisbasement@gmail.com>
2025-06-21 13:40:00 +0300

    per-dimension overlay, fixes #8

--- src/main/java/com/dooji/underlay/main/events/PlayerEvents.java
+++ src/main/java/com/dooji/underlay/main/events/PlayerEvents.java
@@ -10,9 +10,22 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Underlay.MOD_ID)
 public class PlayerEvents {
-
     @SubscribeEvent
     public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            UnderlayNetworking.syncOverlaysToPlayer(player);
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerChangedDim(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            UnderlayNetworking.syncOverlaysToPlayer(player);
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
             UnderlayNetworking.syncOverlaysToPlayer(player);
         }
