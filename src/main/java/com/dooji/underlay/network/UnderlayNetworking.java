74008ce5622cf26ad73633f49f8fcc68944461de
dooji2 <doojisbasement@gmail.com>
2025-08-31 18:05:05 +0300

    fixes #16

--- src/main/java/com/dooji/underlay/network/UnderlayNetworking.java
+++ src/main/java/com/dooji/underlay/network/UnderlayNetworking.java
@@ -111,9 +111,7 @@ public class UnderlayNetworking {
 			tags.put(pos, NbtHelper.fromBlockState(state))
 		);
 
-		if (!tags.isEmpty()) {
-			ServerPlayNetworking.send(player, new SyncOverlaysPayload(tags));
-		}
+		ServerPlayNetworking.send(player, new SyncOverlaysPayload(tags));
 	}
 
 	public static void broadcastAdd(ServerWorld world, BlockPos pos) {
