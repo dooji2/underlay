527921203ae1bc2afcbc7b1af9d4c71fc286c29a
dooji2 <doojisbasement@gmail.com>
2026-06-06 15:14:51 +0300

    fixes #18

---
+++ src/client/java/com/dooji/underlay/jade/JadeComponents.java
@@ -0,0 +1,17 @@
+package com.dooji.underlay.jade;
+
+import net.minecraft.block.Block;
+import snownee.jade.api.IWailaClientRegistration;
+
+public final class JadeComponents {
+    private JadeComponents() {
+    }
+
+    public static void init() {
+        UnderlayJadePlugin.setClientRegistration(JadeComponents::register);
+    }
+
+    private static void register(IWailaClientRegistration registration) {
+        registration.registerBlockComponent(UnderlayComponentProvider.INSTANCE, Block.class);
+    }
+}
--- src/main/java/com/dooji/underlay/jade/UnderlayJadePlugin.java
+++ src/main/java/com/dooji/underlay/jade/UnderlayJadePlugin.java
@@ -1,6 +1,8 @@
 package com.dooji.underlay.jade;
 
-import net.minecraft.block.Block;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
@@ -8,12 +10,19 @@ import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
 public class UnderlayJadePlugin implements IWailaPlugin {
+    private static Consumer<IWailaClientRegistration> clientRegistration = registration -> {
+    };
+
+    public static void setClientRegistration(Consumer<IWailaClientRegistration> clientRegistration) {
+        UnderlayJadePlugin.clientRegistration = Objects.requireNonNull(clientRegistration);
+    }
+
     @Override
     public void register(IWailaCommonRegistration registration) {
     }
 
     @Override
     public void registerClient(IWailaClientRegistration registration) {
-        registration.registerBlockComponent(UnderlayComponentProvider.INSTANCE, Block.class);
+        clientRegistration.accept(registration);
     }
 }
